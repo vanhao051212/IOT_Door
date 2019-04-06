bd2cbefb5155f134f8cf7c622f025317b94f8f0c
Van Hao <43191109+vanhao051212@users.noreply.github.com>
2019-04-07 01:25:45 +0700

    Add files via upload

---
+++ WebServer/Request.php
@@ -0,0 +1,47 @@
+<?php
+	if($_SERVER['REQUEST_METHOD']==='POST'){
+
+		$servername= "localhost";
+		$username = "root";
+		$password = "";
+		$db = "iot_door";
+
+			if(!empty($_POST['Request']) && !empty($_POST['IDPhong']) ){
+			$req =$_POST['Request'];
+			$ID_Phong=$_POST['IDPhong'];
+
+			$conn= new mysqli("localhost", $username, $password, $db );
+			if ($conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+			}
+
+
+
+			//UPDATE `phong` SET `WATER` = 'ON' WHERE `phong`.`IDPhong` = 'E6.1'
+			$sql = "UPDATE phong SET $req= 'ON' where IDPhong= 'E6.1' ";
+			 
+			if ($conn->query($sql) === TRUE) {
+    			echo "Record updated successfully";
+			} else {
+		    echo "Error updating record: " . $conn->error;
+			}
+
+			
+			
+			$conn->close();
+			
+
+		 }
+
+
+
+			
+	}
+
+
+
+
+
+
+
+ ?>
