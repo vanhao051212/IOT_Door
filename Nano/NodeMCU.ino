eef909fc6caadb405176af3452a163062a88fb1c
Van Hao <43191109+vanhao051212@users.noreply.github.com>
2019-04-06 16:57:40 +0700

    Add files via upload

---
+++ Nano/NodeMCU.ino
@@ -0,0 +1,29 @@
+#include"function.h"
+
+
+void setup() {
+  // put your setup code here, to run once:
+  INIT();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(ss.available()>0){
+    char character = char(ss.read());
+    
+    // if receive id
+    if(character != '.'){
+      IDString += character;   
+    }
+    if(character == '.'){
+      Serial.println("Checking id");
+      IDString.trim();  
+      Check_ID(IDString);       
+      IDString="";
+
+    }
+  }
+      
+
+}
--- Nano/function.h
+++ Nano/function.h
@@ -1,97 +1,86 @@
-#include<SoftwareSerial.h>
-#include<LiquidCrystal_I2C.h>
-#include <SPI.h>
-#include <RFID.h>
-#include<Arduino_JSON.h>
-SoftwareSerial ss(9, 8);    // RX(TX-ESP)   TX(RX_ESP)
-LiquidCrystal_I2C lcd(0x27,16,02);
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+#include <Arduino_JSON.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial ss(D5,D1);
+
+
+#define Led1  6
+#define Led2  7
 
 
 
+const char* ssid     = "Van Hao";
+const char* password = "17520451";
+const char* host = "192.168.0.1";
+WiFiClient client;
 
-#define STT1  2
-#define STT2  3
-#define STT3  4
-#define STT4  5
-#define RST   7
-#define Led1  A2
-#define Led2  A3
+String IDString="";
+/* change your link here **********************************/
+String uri = "http://192.168.137.1:3000/checkRFID";
 
-#define SS_PIN 10
-#define RST_PIN 7
-RFID rfid(SS_PIN, RST_PIN);
 
-JSONVar myObject;
-
-unsigned char reading_card[5]; //for reading card
-
-int Time_Out;
 
 void INIT(){
   Serial.begin(9600);
   ss.begin(9600);
-  SPI.begin();
-  rfid.init();
-  lcd.init();
-  lcd.backlight();
-  pinMode(STT1,INPUT_PULLUP);
-  pinMode(STT2,INPUT_PULLUP);
-  pinMode(STT3,INPUT_PULLUP);
-  pinMode(STT4,INPUT_PULLUP);
+  delay(100);
+
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+ while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP()); 
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  Serial.println("connected server");
+
+
   
 }
 
-
-
-
-void Send_ID(unsigned char id[]){
-  myObject["id0"]=id[0];
-  myObject["id1"]=id[1];
-  myObject["id2"]=id[2];
-  myObject["id3"]=id[3];
-  myObject["id4"]=id[4];
-
-  String jsonString = JSON.stringify(myObject);
-  Serial.println(jsonString);
-  //ss.print(jsonString);
-}
-void Check_Button(){
-  if(!digitalRead(STT1)){
-    while(!digitalRead(STT1));
-    Serial.println("YEU CAU MAY CHIEU");
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("DANG YEU CAU:");
-    lcd.setCursor(0,1);
-    lcd.print("MAY CHIEU");
-    ss.print(1);
+void Check_ID(String ID){
+  HTTPClient http;
+  http.begin(uri);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  int httpCode=http.POST(String ("ID=")+ ID); 
+  String payload=http.getString();
+  http.end();   
+  // if successful
+  if(httpCode==200){
+    Serial.println(payload);
+  } 
+  DynamicJsonBuffer jsonBuffer;
+  JsonArray &root = jsonBuffer.parseArray(payload);
+  if (!root.success()) {
+    Serial.println("parseArray() failed");
+  } else {
+    int A=root.size(); //root[index]["Obj"]
+    //Serial.println(A);
+    
   }
-  if(!digitalRead(STT2)){
-    while(!digitalRead(STT2));
-    Serial.println("YEU CAU NUOC");
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("DANG YEU CAU:");
-    lcd.setCursor(0,1);
-    lcd.print("NUOC UONG");
-    ss.print(2);
+  //int check= root[0]["Result"];
+  //Serial.print(check);
+  if(root[0]["Result"]==1){
+    ss.print("success.");
   }
-  if(!digitalRead(STT3)){
-    while(!digitalRead(STT3));
-    Serial.println("YEU CAU MICRO");
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("DANG YEU CAU:");
-    lcd.setCursor(0,1);
-    lcd.print("MICRO");
-    ss.print(3);
-  }
-  if(!digitalRead(STT4)){
-    while(!digitalRead(STT4));
-    Serial.println("BAO VAO LOP");
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("BAO DA VAO LOP");
-    ss.print(4);
+  else{
+    ss.print("fail.");
   }
 }
