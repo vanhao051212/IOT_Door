4d5953aab2df99bad3afadac1c5d3d05cd3eb494
Van Hao <43191109+vanhao051212@users.noreply.github.com>
2019-04-06 23:52:01 +0700

    show request

---
+++ WebServer/Show.php
@@ -0,0 +1,34 @@
+<?php 
+	
+	$servername= "localhost";
+	$username = "root";
+	$password = "";
+	$db = "iot_door";
+	$conn= new mysqli("localhost", $username, $password, $db );
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+
+	$sql = "SELECT * from phong where MIC='ON' or REMOTE='ON' or WATER='ON' or TIME_UP='ON' ";
+	$res = $conn->query($sql);
+	if ($res->num_rows > 0) {
+	// output data of each row
+	    while($row = $res->fetch_assoc()) {
+	        echo "Phong " . $row["ID Phong"]. ":    MIC: " . $row["MIC"]. " - REMOTE: " . $row["REMOTE"]. "-WATER " . $row["REMOTE"]." - TIME_UP: " . $row["TIME_UP"]. "<br>";
+	    }
+	} else {
+	    echo "0 results";
+	}
+	
+	$conn->close();
+
+
+
+
+
+
+
+
+
+
+ ?>
