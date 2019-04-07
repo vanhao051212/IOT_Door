664ae01ab52509c37586baf3ecf92f2cb946d175
Van Hao <43191109+vanhao051212@users.noreply.github.com>
2019-04-07 18:31:06 +0700

    auto refresh page every 10s

--- WebServer/Show.php
+++ WebServer/Show.php
@@ -1,34 +1,36 @@
-<?php 
-	
-	$servername= "localhost";
-	$username = "root";
-	$password = "";
-	$db = "iot_door";
-	$conn= new mysqli("localhost", $username, $password, $db );
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
-
-	$sql = "SELECT * from phong where MIC='ON' or REMOTE='ON' or WATER='ON' or TIME_UP='ON' ";
-	$res = $conn->query($sql);
-	if ($res->num_rows > 0) {
-	// output data of each row
-	    while($row = $res->fetch_assoc()) {
-	        echo "Phong " . $row["ID Phong"]. ":    MIC: " . $row["MIC"]. " - REMOTE: " . $row["REMOTE"]. "-WATER " . $row["REMOTE"]." - TIME_UP: " . $row["TIME_UP"]. "<br>";
-	    }
-	} else {
-	    echo "0 results";
-	}
-	
-	$conn->close();
 
 
 
+ <html>
+    <head>
+	<meta http-equiv="refresh" content="10" > 
+    </head>
+    <body>
+    <?php 
 
 
+		$servername= "localhost";
+		$username = "root";
+		$password = "";
+		$db = "iot_door";
+		$conn= new mysqli("localhost", $username, $password, $db );
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
 
+		$sql = "SELECT * from phong where MIC='ON' or REMOTE='ON' or WATER='ON' or TIME_UP='ON' ";
+		$res = $conn->query($sql);
+		if ($res->num_rows > 0) {
+		// output data of each row
+		    while($row = $res->fetch_assoc()) {
+		        echo "Phong " . $row["IDPhong"]. ":    MIC: " . $row["MIC"]. " - REMOTE: " . $row["REMOTE"]. "-WATER " . $row["WATER"]." - TIME_UP: " . $row["TIME_UP"]. "<br>";
+		    }
+		} else {
+		    echo "0 results";
+		}
+		
+		$conn->close();
 
-
-
-
- ?>
+ 	?>
+    </body>
+</html>
